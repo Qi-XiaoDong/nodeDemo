9dd12ccb7c3a5274dc6eba70a38bd2aa7eed8ca3
qixiaodong <qxdwork@aliyun.com>
2023-05-31 16:16:02 +0800

    tokenMiddleware中间件增加白名单功能 使用path-to-regexp库对请求路径进行匹配，

--- routes/middleware/tokenMiddleware.js
+++ routes/middleware/tokenMiddleware.js
@@ -2,22 +2,35 @@
  * 校验用户是否登录
  */
 const getMsg = require("../utils/getSendResult");
+const { pathToRegexp } = require("path-to-regexp");
 
-const whiteObj = [
+const whiteApis = [
   {
-    method: "post",
-    path: "api/login",
+    method: "get",
+    path: "/api/login",
   },
   {
     method: "post",
-    path: "api/login",
+    path: "/api/login",
   },
   {
     method: "post",
-    path: "api/login",
+    path: "/api/whoami",
   },
 ];
 module.exports = (req, res, next) => {
+  const isWhite = whiteApis.some((api) => {
+    const reg = pathToRegexp(api.path);
+    return (
+      api.method.toLocaleUpperCase() === req.method.toLocaleUpperCase() &&
+      reg.test(req.path)
+    );
+  });
+  if (isWhite) {
+    next();
+    return;
+  }
+
   const token = req.cookies.token || req.headers.authorization;
   if (!token) {
     nonTokenHandle(req, res, next);
